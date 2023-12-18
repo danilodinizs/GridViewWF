e2aa5bce54cf819cdddea2daa77272b82ea46718
Danilo Diniz <danilodiniz93@yahoo.com>
2023-12-18 20:59:32 -0300

    First steps

---
+++ GridView/Welcome.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace GridView
+{
+    partial class Welcome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            txtAdress = new RichTextBox();
+            txtName = new TextBox();
+            txtEmail = new TextBox();
+            txtPassword = new TextBox();
+            txtPhone = new TextBox();
+            txtConfirmPassword = new TextBox();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.DarkTurquoise;
+            button1.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(12, 340);
+            button1.Name = "button1";
+            button1.Size = new Size(202, 72);
+            button1.TabIndex = 0;
+            button1.Text = "Add Button";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(267, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(244, 35);
+            label1.TabIndex = 1;
+            label1.Text = "Student Entry Form";
+            // 
+            // txtAdress
+            // 
+            txtAdress.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            txtAdress.Location = new Point(15, 268);
+            txtAdress.Name = "txtAdress";
+            txtAdress.Size = new Size(199, 64);
+            txtAdress.TabIndex = 2;
+            txtAdress.Text = "";
+            // 
+            // txtName
+            // 
+            txtName.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            txtName.Location = new Point(15, 78);
+            txtName.Name = "txtName";
+            txtName.PlaceholderText = "Full Name";
+            txtName.Size = new Size(199, 32);
+            txtName.TabIndex = 3;
+            // 
+            // txtEmail
+            // 
+            txtEmail.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            txtEmail.Location = new Point(15, 116);
+            txtEmail.Name = "txtEmail";
+            txtEmail.PlaceholderText = "Email";
+            txtEmail.Size = new Size(199, 32);
+            txtEmail.TabIndex = 4;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            txtPassword.Location = new Point(15, 192);
+            txtPassword.Name = "txtPassword";
+            txtPassword.PasswordChar = '*';
+            txtPassword.PlaceholderText = "Password";
+            txtPassword.Size = new Size(199, 32);
+            txtPassword.TabIndex = 6;
+            // 
+            // txtPhone
+            // 
+            txtPhone.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            txtPhone.Location = new Point(15, 154);
+            txtPhone.Name = "txtPhone";
+            txtPhone.PlaceholderText = "Phone";
+            txtPhone.Size = new Size(199, 32);
+            txtPhone.TabIndex = 7;
+            // 
+            // txtConfirmPassword
+            // 
+            txtConfirmPassword.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            txtConfirmPassword.Location = new Point(15, 230);
+            txtConfirmPassword.Name = "txtConfirmPassword";
+            txtConfirmPassword.PasswordChar = '*';
+            txtConfirmPassword.PlaceholderText = "Confirm Password";
+            txtConfirmPassword.Size = new Size(199, 32);
+            txtConfirmPassword.TabIndex = 8;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(243, 78);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(545, 334);
+            dataGridView1.TabIndex = 9;
+            // 
+            // Welcome
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.PaleTurquoise;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dataGridView1);
+            Controls.Add(txtConfirmPassword);
+            Controls.Add(txtPhone);
+            Controls.Add(txtPassword);
+            Controls.Add(txtEmail);
+            Controls.Add(txtName);
+            Controls.Add(txtAdress);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Welcome";
+            Text = "Welcome";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private RichTextBox txtAdress;
+        private TextBox txtName;
+        private TextBox txtEmail;
+        private TextBox txtPassword;
+        private TextBox txtPhone;
+        private TextBox txtConfirmPassword;
+        private DataGridView dataGridView1;
+    }
+}
---
+++ GridView/Welcome.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GridView
+{
+    public partial class Welcome : Form
+    {
+        public Welcome()
+        {
+            InitializeComponent();
+            try
+            {
+                SqlConnection connection = new SqlConnection("Data Source=.;Initial Catalog=GridView;Integrated Security=True");
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Students", connection);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Something was wrong, please try again\nErrorTrace:" + ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection("Data Source=.;Initial Catalog=GridView;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("INSERT INTO Students VALUES('" + txtName.Text + "','" + txtEmail.Text + "','" + txtPhone.Text + "','" + txtAdress.Text + "',)", connection);
+        }
+    }
+}
